a3fbf464ee67ada896a922792d5bec4516855456
Kieran Wake <kieranwake1234@gmail.com>
2024-02-02 15:16:23 +0000

    testing different values

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const a = 5; 
-const b = 2; 
+const a = 6; 
+const b = 3; 
 
 const resultShift = a << b; 
 
 console.log(resultShift); 
 
-const resultMultiplication = a * Math.pow(2, b); 
+const resultMultiplication = a * Math.pow(3, b); 
 
 console.log(resultMultiplication); 
